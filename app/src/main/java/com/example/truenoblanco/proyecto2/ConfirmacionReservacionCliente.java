0948519a0ab1005cf494fa3558aa9f9b17d39089
Rafael <antdhv365@gmail.com>
2016-06-04 15:21:32 -0600

    Pago por Paypal agregado

--- app/src/main/java/com/example/truenoblanco/proyecto2/ConfirmacionReservacionCliente.java
+++ app/src/main/java/com/example/truenoblanco/proyecto2/ConfirmacionReservacionCliente.java
@@ -1,18 +1,40 @@
 package com.example.truenoblanco.proyecto2;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.truenoblanco.proyecto2.login.Transaccion;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.example.truenoblanco.proyecto2.login.UtilPayPal;
+
+import java.math.BigDecimal;
 
 public class ConfirmacionReservacionCliente extends AppCompatActivity {
 
     String persona, habitacion, dias, promo, tipoHabitacion,fecha,precio;
     TextView mensaje,mensaje2,mensaje3;
+
+    private static final int REQUEST_CODE_PAYMENT = 1;
+
+    private static PayPalConfiguration paypalConfig=new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            .clientId( UtilPayPal.paypal_sdk_id)
+            .acceptCreditCards(true)
+                    // The following are only used in PayPalFuturePaymentActivity.
+            .merchantName("Code_Crash")
+            .merchantPrivacyPolicyUri(
+                    Uri.parse("https://www.paypal.com/webapps/mpp/ua/privacy-full"))
+            .merchantUserAgreementUri(
+                    Uri.parse("https://www.paypal.com/webapps/mpp/ua/useragreement-full"));
 
 
     @Override
@@ -20,11 +42,22 @@ public class ConfirmacionReservacionCliente extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmacion_reservacion_cliente);
 
-
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, paypalConfig);
+        startService(intent);
 
         mensaje = (TextView)findViewById(R.id.tvReservaConfor);
         mensaje2 = (TextView) findViewById(R.id.Precio);
         mensaje3=(TextView) findViewById(R.id.precio2);
+
+        Button confirmar = (Button) findViewById(R.id.ButtonConfirmar);
+        confirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pagar();
+            }
+        });
 
         Bundle bundle = getIntent().getExtras();
 
@@ -40,7 +73,6 @@ public class ConfirmacionReservacionCliente extends AppCompatActivity {
         mensaje2.setText("CON UN TOTAL A PAGAR DE");
         mensaje3.setText("$ "+precio);
 
-
     }
 
     public void iniciar(View v){
@@ -49,6 +81,40 @@ public class ConfirmacionReservacionCliente extends AppCompatActivity {
 
     }
 
+    public void pagar(){
 
+        PayPalPayment pago = new PayPalPayment(new BigDecimal(precio),"USD", "Hotel El Papu",
+                PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent intent = new Intent(ConfirmacionReservacionCliente.this, PaymentActivity.class);
+
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, paypalConfig);
+
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, pago);
+
+        startActivityForResult(intent, REQUEST_CODE_PAYMENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                Toast.makeText(getApplicationContext(), "Pago realizado correctamente", Toast.LENGTH_LONG).show();
+
+            }
+
+            else if (resultCode == Activity.RESULT_CANCELED) {
+
+                Toast.makeText(getApplicationContext(), "Pago cancelado. Intente de nuevo", Toast.LENGTH_LONG).show();
+
+
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+
+                Toast.makeText(getApplicationContext(), "Pago cancelado. Intente de nuevo", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/truenoblanco/proyecto2/login/UtilPayPal.java
@@ -0,0 +1,14 @@
+package com.example.truenoblanco.proyecto2.login;
+
+/**
+ * Created by Rafael Pineda on 03/06/2016.
+ */
+public class UtilPayPal {
+
+    public  static  final  String sand_box_id="APP-80W284485P519543T";
+    public  static  final  String paypal_liv_id="APP-0C165959RL117014F";
+
+    //public  static  final  String paypal_sdk_id="AZLO_x33AwUGmMcL354-ufTI7G-aUy0UXJXpE8n430XeWYjb13nvv0CKjcg8o89Agyt_C3Ef8j2tC9so";
+    public  static  final  String paypal_sdk_id="AQmq3R1p7BIE5vOoGljyJGqebsd4it7zr-XhO1CBL7GxKN-KFP4qBlo6qlx6m76NNiw0rnyO9HO3avb1";
+
+}
